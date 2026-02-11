f140bea1cbe3613422d998a940f68f592de4ebac
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-02-11 22:32:55 +0530

    Update voice.py

--- javis voice assist/voice.py
+++ javis voice assist/voice.py
@@ -9,7 +9,6 @@ import time
 
 
  
-
 def speechtotxt():
     recognizer = sr.Recognizer()   #Recognizer class speech_recognition me hota jo sab speech ko appne pass rkhta h
     with sr.Microphone() as source:  
@@ -79,5 +78,6 @@ if __name__ == '__main__':  # ye programm ko split kr dega yeha se do
 else:
 
     print("thank")
+
 
 
