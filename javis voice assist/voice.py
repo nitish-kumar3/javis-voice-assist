d1b8d75bf44685db33d449a062e528c12b06e4f3
Nitish Kumar <163245178+nitish-kumar3@users.noreply.github.com>
2026-04-02 23:25:29 +0530

    Update voice.py

--- javis voice assist/voice.py
+++ javis voice assist/voice.py
@@ -5,7 +5,6 @@ import datetime
 import pyjokes
 import time
 
-
 def speechtotxt():
     recognizer = sr.Recognizer()   #Recognizer class speech_recognition me hota jo sab speech ko appne pass rkhta h
     with sr.Microphone() as source:  
